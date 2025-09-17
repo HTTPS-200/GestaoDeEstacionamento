a093a6dab7e492c02b50492e97815a8ed104542d
Vicente Junior <vicente_pc@outlook.com>
2025-09-16 22:33:10 -0300

    Implementado InterfaceFatura que faltava no Dominio Implementado Inteface e Mapeador ModuloFaturamento - Infraestrutura

---
+++ Server/GestaoDeEstacionamento.Infraestrutura.Orm/ModuloFaturamento/MapeadorFatura.cs
@@ -0,0 +1,32 @@
+﻿using GestaoDeEstacionamento.Core.Dominio.ModuloFaturamento;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GestaoDeEstacionamento.Infraestrutura.Orm.ModuloFaturamento;
+public class MapeadorFatura : IEntityTypeConfiguration<Fatura>
+{
+    public void Configure(EntityTypeBuilder<Fatura> builder)
+    {
+        builder.ToTable("faturas");
+
+        builder.HasKey(f => f.Id);
+
+        builder.Property(f => f.TicketId)
+            .IsRequired();
+
+        builder.Property(f => f.DataEntrada)
+            .IsRequired();
+
+        builder.Property(f => f.DataSaida)
+            .IsRequired();
+
+        builder.Property(f => f.Diarias)
+            .IsRequired();
+
+        builder.Property(f => f.ValorDiaria)
+            .HasColumnType("decimal(10,2)")
+            .IsRequired();
+
+        builder.Ignore(f => f.ValorTotal); // Ignorar propriedade calculada para evitar erros na migrations
+    }
+}
---
+++ Server/GestaoDeEstacionamento.Infraestrutura.Orm/ModuloFaturamento/RepositorioFaturamentoEmOrm.cs
@@ -0,0 +1,6 @@
+﻿using GestaoDeEstacionamento.Core.Dominio.ModuloFaturamento;
+using GestaoDeEstacionamento.Infraestrutura.Orm.Compartilhado;
+
+namespace GestaoDeEstacionamento.Infraestrutura.Orm.ModuloFaturamento;
+public class RepositorioFaturamentoEmOrm(AppDbContext contexto)
+    : RepositorioBaseEmOrm<Fatura>(contexto), IRepositorioFaturamento;
