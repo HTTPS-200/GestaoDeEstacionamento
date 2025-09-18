57a3cea0ba80af72e8102137997b9ac5c8445697
Vicente Junior <vicente_pc@outlook.com>
2025-09-18 17:01:02 -0300

    Implementado EditarCommand e Request Modulo Vaga

---
+++ Server/GestaoDeEstacionamento.Core.Aplicacao/ModuloGestaoDeVagas/Command/EditarContatoCommand.cs
@@ -0,0 +1,19 @@
+﻿using FluentResults;
+using GestaoDeEstacionamento.Core.Dominio.ModuloCheckIn;
+using MediatR;
+
+namespace GestaoDeEstacionamento.Core.Aplicacao.ModuloGestaoDeVagas.Command;
+public record EditarVagaCommand(
+    Guid id,
+    string NomeDaVaga,
+    string Zona,
+    bool Ocupada,
+    Veiculo? Veiculo
+    ) : IRequest<IResult<EditarVagaResult>>;
+
+public record EditarVagaResult(
+    string NomeDaVaga,
+    string Zona,
+    bool Ocupada,
+    Veiculo? Veiculo
+    );
