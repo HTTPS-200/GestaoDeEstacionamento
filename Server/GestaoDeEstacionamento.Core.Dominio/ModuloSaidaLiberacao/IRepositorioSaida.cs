8035ea4574be26f31a0cafabf589a501f8b03a9d
davidinovargas <davidinovargas@gmail.com>
2025-09-22 09:30:22 -0300

    Deletado Arquivos Desnecessários

--- Server/GestaoDeEstacionamento.Core.Dominio/ModuloSaidaLiberacao/IRepositorioSaida.cs
+++
@@ -1,9 +0,0 @@
-﻿using GestaoDeEstacionamento.Core.Dominio.Compartilhado;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GestaoDeEstacionamento.Core.Dominio.ModuloSaidaLiberacao;
-public interface IRepositorioSaida : IRepositorio<Saida>;
